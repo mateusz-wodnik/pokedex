c2b13cd013c1e8a4c5bf0be1bdebf965caf17b36
mateusz-wodnik <mateusz.wodnik@gmail.com>
2018-09-24 04:19:52 +0200

    Add bootstrap 4, Remove css reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'; // eslint-disable-line
+import bs from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.sass';
 
+
 const App = () => (
-  <div className={styles.container}>
+  <div className={`${bs.container} ${styles.container}`}>
     React App
+    <button type="button" className={`${bs.btn} ${bs['btn-primary']}`}>eleoeleoeeoeol</button>
   </div>
 );
 
