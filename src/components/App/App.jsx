cc1b130b471a6988ade05f6ead92da0ca63ef017
mateusz-wodnik <mateusz.wodnik@gmail.com>
2018-09-24 05:09:04 +0200

    Update files structure, Cleanup src directory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'; // eslint-disable-line
-import bs from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bs from 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './App.sass';
 
 
 const App = () => (
   <div className={`${bs.container} ${styles.container}`}>
     React App
-    <button type="button" className={`${bs.btn} ${bs['btn-primary']}`}>eleoeleoeeoeol</button>
   </div>
 );
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.sass';
-import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
