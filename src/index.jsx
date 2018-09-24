04dd1a5bbf1e8558233b541fe5bc0b1480a08a73
mateusz-wodnik <mateusz.wodnik@gmail.com>
2018-09-24 05:29:25 +0200

    Add redux, Create main store, Add middlewares, Connect reducers

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.sass';
-import App from './components/App/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Components
+import App from './components/App/App';
+
+// Service Worker
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'),
+);
+
 registerServiceWorker();
