096422565d63dbe3e8780e5be5897e498c93987e
mateusz-wodnik <mateusz.wodnik@gmail.com>
2018-09-25 04:19:40 +0200

    Add item content

--- src/components/PokemonList/components/Item/Item.jsx
+++ src/components/PokemonList/components/Item/Item.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import bs from 'bootstrap/dist/css/bootstrap.min.css';
+import bs from 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './Item.sass';
 
 
-const PokemonItem = ({ item }) => (
-  <li className={`${styles.container}`}></li>
+const PokemonItem = ({
+  id,
+  img,
+  num,
+  name,
+  type,
+}) => (
+  <li className={`${styles.container} ${bs.card}`}>
+    <img className={bs['card-img-top']} src={img} alt={name} />
+    <div className={bs['card-body']}>
+      <h2 className={bs['card-title']}>{`#${num} ${name}`}</h2>
+      <ul className={styles.types}></ul>
+    </div>
+  </li>
 );
 
 PokemonItem.defaultProps = {
-  item: {},
+  id: 'No id',
+  img: '/placeholder.jpg',
+  num: 'No number',
+  name: 'No name',
+  type: [],
 };
 
 PokemonItem.propTypes = {
-  item: PropTypes.object,
+  id: PropTypes.string,
+  img: PropTypes.string,
+  num: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default PokemonItem;
